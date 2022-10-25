22c3325e9cd3c0100f8a58fa1d73d42f5601288f
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-25 10:32:53 +0530

    Published

---
+++ index.js
@@ -0,0 +1,54 @@
+//Add note
+
+let add = document.querySelector("#add");
+
+add.addEventListener("click", () => {
+  let noteobj = [];
+  let addtxt = document.getElementById("note");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    noteobj = [];
+  } else {
+    noteobj = JSON.parse(localStorage.getItem("notes"));
+  }
+  if (addtxt.value != "") {
+    noteobj.push(addtxt.value);
+  }
+
+  localStorage.setItem("notes", JSON.stringify(noteobj));
+
+  showNotes();
+});
+
+function showNotes() {
+  let notesobj = JSON.parse(localStorage.getItem("notes"));
+  console.log(notesobj);
+  if (notesobj == null) {
+    notesobj = [];
+  } else {
+    notesobj = JSON.parse(localStorage.getItem("notes"));
+  }
+  let html = "";
+  notesobj.forEach(function (element, index) {
+    html += `<div id="box" >
+    <h5>NOTE :${index + 1}</h5>
+    <p>${element.toUpperCase()}</p>
+    <button id=delete onclick=deleted(${index})>Delete note</button>
+        </div>`;
+  });
+  let box = document.getElementById("mainbox");
+  box.innerHTML = html;
+}
+
+function deleted(index) {
+  console.log("Deletion of " + index + " success");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    noteobj = [];
+  } else {
+    noteobj = JSON.parse(localStorage.getItem("notes"));
+  }
+  noteobj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(noteobj));
+  showNotes();
+}
