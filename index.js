7eaa1ac92b911ebd94bce02e3cbafbd5bf9bf8f7
khushigarg1 <khushigarg.64901@gmail.com>
2022-10-26 13:10:15 +0530

    dark mode

--- index.js
+++ index.js
@@ -5,143 +5,145 @@ let noInput = document.querySelector("#noInputText");
 let noteInput = document.getElementById("note");
 
 noteInput.addEventListener("click", function () {
-  noInput.classList.remove("show");
+    noInput.classList.remove("show");
 });
 
 formContainer.addEventListener("submit", event => {
-  event.preventDefault()
-  let noteobj = [];
-  let addtxt = document.getElementById("note");
-  let notes = localStorage.getItem("notes");
+    event.preventDefault()
+    let noteobj = [];
+    let addtxt = document.getElementById("note");
+    let notes = localStorage.getItem("notes");
 
-  if (notes == null) {
-    noteobj = []
-  }
-   else {
-    noteobj = JSON.parse(localStorage.getItem("notes"));
-  }
+    if (notes == null) {
+        noteobj = []
+    }
+    else {
+        noteobj = JSON.parse(localStorage.getItem("notes"));
+    }
 
-  if (addtxt.value != "") {
-    noteobj.push(addtxt.value);
-  } 
-  else {
-    noInput.classList.add("show");
-  }
+    if (addtxt.value != "") {
+        noteobj.push(addtxt.value);
+    }
+    else {
+        noInput.classList.add("show");
+    }
 
-  localStorage.setItem("notes", JSON.stringify(noteobj));
-  noteInput.value = ""
+    localStorage.setItem("notes", JSON.stringify(noteobj));
+    noteInput.value = ""
 
-  showNotes();
+    showNotes();
 
 });
 
 function showNotes() {
-  let notesobj = JSON.parse(localStorage.getItem("notes"));
-  console.log(notesobj);
+    let notesobj = JSON.parse(localStorage.getItem("notes"));
+    console.log(notesobj);
 
-  if (notesobj == null) {
-    notesobj = [];
-  } 
-  else {
-    notesobj = JSON.parse(localStorage.getItem("notes"));
-  }
+    if (notesobj == null) {
+        notesobj = [];
+    }
+    else {
+        notesobj = JSON.parse(localStorage.getItem("notes"));
+    }
 
-  let html = "";
-  notesobj.forEach(function (element, index) {
-    html += `<div id="box" >
+    let html = "";
+    notesobj.forEach(function (element, index) {
+        html += `<div id="box" >
     <h5>NOTE :${index + 1}</h5>
     <p>${element.toUpperCase()}</p>
     <button id=delete onclick=deleted(${index})>Delete note</button>
         </div>`;
-  });
+    });
 
-  let box = document.getElementById("mainbox");
-  box.innerHTML = html;
+    let box = document.getElementById("mainbox");
+    box.innerHTML = html;
 
 }
 
 function deleted(index) {
-  console.log("Deletion of " + index + " success");
-  let notes = localStorage.getItem("notes");
+    console.log("Deletion of " + index + " success");
+    let notes = localStorage.getItem("notes");
 
-  if (notes == null) {
-    noteobj = [];
-  } 
-  else {
-    noteobj = JSON.parse(localStorage.getItem("notes"));
-  }
-  noteobj.splice(index, 1);
-  localStorage.setItem("notes", JSON.stringify(noteobj));
-  showNotes();
-  
+    if (notes == null) {
+        noteobj = [];
+    }
+    else {
+        noteobj = JSON.parse(localStorage.getItem("notes"));
+    }
+    noteobj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(noteobj));
+    showNotes();
+
 }
 
 
 
 
 
-document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
+document.body.style = "background-color: var(--bs-dark);transition: 0.5s;"
 const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
 const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
 
 var theme = "dark";
-  const root = document.querySelector(":root");
-  const container = document.getElementsByClassName("theme-container")[0];
-  const themeIcon = document.getElementById("theme-icon");
-  const texthead = document.getElementById("text");
-  container.addEventListener("click", setTheme);
-  function setTheme() {
+const root = document.querySelector(":root");
+const container = document.getElementsByClassName("theme-container")[0];
+const themeIcon = document.getElementById("theme-icon");
+const texthead = document.getElementById("text");
+container.addEventListener("click", setTheme);
+function setTheme() {
     switch (theme) {
-      case "dark":
-        setLight();
-        theme = "light";
-        break;
-      case "light":
-        setDark();
-        theme = "dark";
-        break;
+        case "dark":
+            setLight();
+            theme = "light";
+            break;
+        case "light":
+            setDark();
+            theme = "dark";
+            break;
     }
-  }
-  function setLight() {
+}
+function setLight() {
     root.style.setProperty(
-      "--bs-dark",
-      "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
+        "--bs-dark", "rgb(111, 143, 175)"
+        //   "linear-gradient(318.32deg, #7e6ce4 0%, #8f84c9 55%, #8f84c9 100%)"
     );
     texthead.style.setProperty(
-        "color","black"
+        "color", "rgb(74, 99, 124)"
     );
     container.classList.remove("shadow-dark");
     setTimeout(() => {
-      container.classList.add("shadow-light");
-      themeIcon.classList.remove("change");
+        container.classList.add("shadow-light");
+        themeIcon.classList.remove("change");
     }, 300);
     themeIcon.classList.add("change");
     themeIcon.src = sun;
-  }
-  function setDark() {
+}
+function setDark() {
     texthead.style.setProperty(
-        "color","white"
+        "color", "#DDA0DD"
     );
-    root.style.setProperty("--bs-dark", "black");
+    root.style.setProperty(
+        "--bs-dark", "rgb(216,191,216,0.5)"
+    );
     container.classList.remove("shadow-light");
     setTimeout(() => {
-      container.classList.add("shadow-dark");
-      themeIcon.classList.remove("change");
+        container.classList.add("shadow-dark");
+        themeIcon.classList.remove("change");
     }, 300);
     themeIcon.classList.add("change");
     themeIcon.src = moon;
-  }
+}
 
-  function edit(index) {
+function edit(index) {
     // edits the value to the value in text area
     console.log("edit of " + index + " success");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
-      noteobj = [];
+        noteobj = [];
     } else {
-      noteobj = JSON.parse(localStorage.getItem("notes"));
+        noteobj = JSON.parse(localStorage.getItem("notes"));
     }
-    noteobj[index]=document.getElementById("note").value;
+    noteobj[index] = document.getElementById("note").value;
     localStorage.setItem("notes", JSON.stringify(noteobj));
     showNotes();
-  }
+}
