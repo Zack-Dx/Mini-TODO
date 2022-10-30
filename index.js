ae57530f68cb3fe74f3420b7a2fcd799690d808c
Boshra Jaber <boshrajaber94@gmail.com>
2022-10-30 10:03:39 +0000

    Conflict has been resolved

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 let formContainer = document.querySelector('#container');
 let noInput = document.querySelector('#noInputText');
 let noteInput = document.getElementById('note');
+const deleteBtn = document.getElementsByClassName('delete-all')[0]
 
 noteInput.addEventListener('click', function () {
   noteInput.classList.remove('show');
@@ -33,6 +34,29 @@ formContainer.addEventListener('submit', (event) => {
 
   showNotes();
 });
+deleteBtn.addEventListener('click', () => {
+  const notes = localStorage.getItem("notes");
+  if (notes === null) {
+    showmsg("No notes to delete");
+    return;
+  }
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete all notes!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      localStorage.removeItem("notes");
+      showmsg('All notes deleted successfully.');
+      document.getElementById('mainbox').innerHTML = "";
+    }
+  })
+})
+
 
 function showNotes() {
   let notesobj = JSON.parse(localStorage.getItem('notes'));
@@ -84,8 +108,9 @@ function edit(index) {
   if (editButton.innerHTML == 'Edit') {
     swappableElement.innerHTML = `
       <div id="notebox">
-        <input type="text" id="note" value="${noteElement.getElementsByTagName('p')[0].innerHTML
-      }" style="width:${noteElement.getElementsByTagName('p')[0].clientWidth + "px"}"/>
+        <input type="text" id="note" value="${
+          noteElement.getElementsByTagName('p')[0].innerHTML
+        }" style="width:${noteElement.getElementsByTagName('p')[0].clientWidth + "px"}"/>
       </div>
     `;
     editButton.innerHTML = 'Save';
@@ -112,16 +137,16 @@ function copyText(index) {
 
 
 let searchtext = document.getElementById('searching');
-searchtext.addEventListener("input", function () {
-  let inputvalue = searchtext.value.toLowerCase();
-  let notecard = document.getElementsByClassName('box');
-  Array.from(notecard).forEach(function (element) {
+searchtext.addEventListener("input", function(){
+   let inputvalue = searchtext.value.toLowerCase();
+   let notecard = document.getElementsByClassName('box');
+   Array.from(notecard).forEach(function(element){
     let cardtext = element.getElementsByTagName('div')[0].getElementsByTagName("p")[0].innerText.toLowerCase();
     if (cardtext.includes(inputvalue)) {
       element.style.display = "inline-block";
-    }
-    else {
+    }  
+    else{
       element.style.display = "none";
     }
-  })
-})
+   })
+ })
